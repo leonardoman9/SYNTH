f703a2fa1cf110c339f8e9db8454f49ada788375
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2022-05-20 19:33:21 +0200

    ADSR

--- leoSynth/leoSynth/Source/SynthVoice.h
+++ leoSynth/leoSynth/Source/SynthVoice.h
@@ -18,9 +18,20 @@ public:
     void startNote(int midiNoteNumber, float velocity, juce::SynthesiserSound *sound, int currentPitchWheelPosition) override;
     void stopNote(float velocity, bool allowTailOff) override;
     void controllerMoved(int controllerNumber, int newControllerValue) override;
+    void prepareToPlay(double sampleRate, int samplesPerBlock, int outputChannels);
     void renderNextBlock(juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override;
     void pitchWheelMoved(int newPitchWheelValue) override;
 
 private:
-    
+    juce::ADSR adsr;
+    juce::ADSR::Parameters adsrParams;
+
+    juce::dsp::Oscillator<float> osc{ [](float x) {return x < 0.0f ? -1.0f : 1.0f; ;  } };
+    juce::dsp::Gain<float> gain;
+    bool isPrepared{ false };
+    /*
+    * return std::sin(x);                           SINE WAVE
+    return x / MathConstants<float>::pi;            SAW WAVE
+    return x < 0.0f ? -1.0f : 1.0f;                 SQUARE WAVE
+    */
 };
